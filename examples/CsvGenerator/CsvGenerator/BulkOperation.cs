ae47e4332a5d551b0f840243e2d334c5d3c7df8b
TareqNewazShahriar <tareq.newaz.shahriar@gmail.com>
2020-11-21 13:34:23 +0600

    refactoring

---
+++ examples/CsvGenerator/CsvGenerator/BulkOperation.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+/// <summary>
+/// Bulk Operaion
+/// Author: Tareq Newaz Shahriar
+/// Licence: Apache License 2.0
+/// Repo: github.com/TareqNewazShahriar/Bulk-Operation-Using-CSharp-Reflection
+/// </summary>
+namespace BulkOperation
+{
+    /// <summary>
+    /// A class to traverse and to do operations on a collection of complex objects.
+    /// It will prepare the property information and values of each property using reflexion
+    /// for the operation.
+    /// </summary>
+    /// <typeparam name="T">Complex type to process</typeparam>
+    /// <typeparam name="TResult">Resultant type</typeparam>
+    public class BulkOperation<T, TResult> 
+		where T : class
+		where TResult : class
+	{
+		#region Fields
+
+		private List<T> list = null;
+		private List<PropertyInfo> properties = null;
+		private List<TResult> processedItems = null;
+
+		Func<List<object>, List<PropertyInfo>, TResult> _ProcessEachItem;
+		Func<List<TResult>, List<PropertyInfo>, TResult> _Finally;
+
+		#endregion Fields
+
+		#region Constructor
+
+		public BulkOperation(List<T> list, Func<List<object>, List<PropertyInfo>, TResult> processEachItems, Func<List<TResult>, List<PropertyInfo>, TResult> AtEnd)
+		{
+			this._ProcessEachItem = processEachItems;
+			this._Finally = AtEnd;
+
+			this.list = list;
+			properties = new List<PropertyInfo>();
+			processedItems = new List<TResult>();
+
+			InitPropObject(typeof(T));
+		}
+
+		#endregion Constructor
+
+		#region Public Methods
+
+		public TResult Process()
+		{
+			foreach (var item in list)
+			{
+				processedItems.Add(_ProcessEachItem(GetValues(item), properties));
+			}
+
+			return _Finally.Invoke(processedItems, properties);
+		}
+
+		#endregion Public Methods
+
+		#region Private Methdos
+
+		private void InitPropObject(Type _type)
+		{
+			// take only native and public proeperties
+			properties = _type.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly).ToList();
+		}
+
+		private List<object> GetValues(T obj)
+		{
+			object val = null;
+			var valueList = new List<object>();
+			// get values from the model
+			foreach (var s in properties)
+			{
+				try
+				{
+					val = s.GetValue(obj) ?? DBNull.Value;
+				}
+				catch
+				{
+					val = DBNull.Value;
+				}
+
+				valueList.Add(val);
+			}
+
+			return valueList;
+		}
+
+		#endregion Private Methods
+	}
+
+}
---
+++ examples/CsvGenerator/CsvGenerator/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace BulkOperation
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var list = new List<Model>();
+			for (int i = 0; i < 10; i++)
+			{
+				list.Add(new Model() { Id = i, Name = "Name-" + i.ToString(), DOB = DateTime.UtcNow });
+			}
+
+			var bulk = new BulkOperation<Model, string>(list, CreateCsvRow, GenerateCsvWithHeader);
+			string resultantData = bulk.Process();
+
+			Console.Write(resultantData);
+		}
+
+		static string CreateCsvRow(List<object> valueList, List<PropertyInfo> props)
+		{
+			string line = "";
+			foreach (var item in valueList)
+			{
+				try
+				{
+					if (item.GetType() == typeof(string))
+						line += "\"" + item.ToString() + "\"";
+					else
+						line += (item ?? "").ToString();
+				}
+				catch { }
+				finally { line += ','; }
+			}
+			return line.TrimEnd(',');
+		}
+
+		static string GenerateCsvWithHeader(List<string> processedList, List<PropertyInfo> props)
+		{
+			StringBuilder stringBuilder = new StringBuilder();
+			props.ForEach(x => stringBuilder.Append(x.Name + ","));
+			stringBuilder.Remove(stringBuilder.Length - 1, 1);
+			stringBuilder.AppendLine();
+
+			foreach (var item in processedList)
+			{
+				stringBuilder.AppendLine(item.ToString());
+			}
+
+			return stringBuilder.ToString();
+		}
+	}
+
+	class Model
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public DateTime DOB { get; set; }
+	}
+}
